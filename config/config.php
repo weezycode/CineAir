ca463a8faac6e9dcc7116fb2b7ea8bd1a3e64e47
Moudou Dicko <42220417+weezycode@users.noreply.github.com>
2022-01-29 03:40:49 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,1 @@
-<?php
-// Connexion à la base de données
-try {
-        $bd = new PDO(
-                'mysql:host=db5002510161.hosting-data.io;dbname=dbs1996872;charset=utf8',
-                'dbu1424581',
-                'gnBZN9eXRk4q@sQ',
-                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-        );
-} catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-}
+
